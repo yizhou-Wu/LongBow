dd29625d68dc6b18ccb7b091361aff2f4f83d04a
Jingdi Zhang <jingdizhang@jingdis-air.lan>
2024-04-06 00:14:43 -0700

    update image overlay and add button to switch between images

--- src/ImageSlider.tsx
+++ src/ImageSlider.tsx
@@ -1,85 +1,101 @@
-// import React from 'react';
-import { useState, useEffect } from "react";
-
-const slides = [
-  { url: "http://localhost:5173/Floor1.jpg", title: "floor" },
-  { url: "http://localhost:5173/Room1.jpg", title: "room" },
-  { url: "http://localhost:5173/Handle1.jpg", title: "handle" },
-];
+import { useEffect, useState } from "react";
 
 const ImageSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const imageContainerStyle = {
-    width: "100%",
-    height: "100%",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    borderRadius: "5%",
-    backgroundImage: `url(${slides[currentIndex].url})`,
-    transition: "background-image 0.5s ease-in-out",
-  };
-  const goToSlide = (slideIndex: number) => {
-    setCurrentIndex(slideIndex);
-  };
+  const images = [
+    "http://localhost:5173/Floor1.jpg",
+    "http://localhost:5173/Room1.jpg",
+    "http://localhost:5173/Handle1.jpg",
+  ];
+
   useEffect(() => {
     // Automatically change slide every 3 seconds (adjust as needed)
     const interval = setInterval(() => {
-      const nextIndex = (currentIndex + 1) % slides.length;
+      const nextIndex = (currentIndex + 1) % images.length;
       setCurrentIndex(nextIndex);
-    }, 3000);
+    }, 5000);
 
     return () => {
       // Clear the interval when the component is unmounted
       clearInterval(interval);
     };
   }, [currentIndex]);
+
+  const goToPrevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
   return (
-    <div className="image-slider-container">
-      <div className="image-slider">
-        <div style={imageContainerStyle}></div>
-        <div className="dot-container">
-          {slides.map((slide, slideIndex) => (
-            <div
-              className="dot-index"
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-            >
-              •
-            </div>
-          ))}
-        </div>
-      </div>
+    <>
+      <button className="slider-button prev" onClick={goToPrevSlide}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+        </svg>
+      </button>
+      <button className="slider-button next" onClick={goToNextSlide}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+        </svg>
+      </button>
+      <img
+        className="slider-image"
+        src={images[currentIndex]}
+        alt={`Slide ${currentIndex + 1}`}
+      />
       <style jsx>
         {`
-          .image-slider-container {
-            display: flex;
-            height: 1000px;
-            align-items: center;
+          .image-slider {
+            position: relative;
+            max-height: 100vh;
+            margin-top: 50px;
           }
 
-          .image-slider {
-            display: inline-block;
-            height: 700px;
-            width: 1000px;
-            margin: 0 auto;
-            align-items: center;
-            justify-content: center;
+          .slider-button {
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            background: transparent;
+            border: none;
+            cursor: pointer;
+            padding: 10px;
+            height: 25px;
+            width: 25px;
           }
-          .dot-container {
-            display: flex;
-            padding-top: 20px;
-            justify-content: center;
+
+          .prev {
+            left: 2.5rem;
           }
-          .dot-index {
-            font-size: 50px;
-            margin: 20px;
-            padding-left: 10px;
-            padding-right: 10px;
-            color: grey;
+
+          .next {
+            right: 2.5rem;
+          }
+
+          .slider-image {
+            width: 100%;
+            height: auto;
+            max-height: 100vh;
           }
         `}
       </style>
-    </div>
+    </>
   );
 };
+
 export default ImageSlider;
